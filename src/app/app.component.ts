714be5d740ec3d7e7da54ba2b09a4ad6bfd58f08
Abdullah Muhammed <abdo56668@gmail.com>
2019-05-19 16:59:08 +0200

    delete test code from master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { User } from './models/user.model';
-import { environment } from 'src/environments/environment';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Galees, Matching Web Application';
-  user: User;
-
-  constructor(private http: HttpClient){}
-
-  ngOnInit(){
-    this.http.get<User>(environment.serverUrl+"auth").subscribe(
-      (data) => {
-        this.user = data;
-      }
-    )
-  }
 }
