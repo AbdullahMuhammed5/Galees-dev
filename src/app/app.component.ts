a48779eab93d9320855072cde1db84e3cb50dfb9
Abdullah Muhammed <abdo56668@gmail.com>
2019-05-19 13:19:41 +0200

    make branches

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './models/user.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Galees, Matching Web Application';
+  user: User;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.http.get<User>(environment.serverUrl+"auth").subscribe(
+      (data) => {
+        this.user = data;
+      }
+    )
+  }
 }
