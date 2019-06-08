86176d9e811fc9e4cc61300ffb3e04d51397a36f
MohammedMahmoudAbdelKarim <mohammedmahmoudabdelkarim@gmail.com>
2019-06-08 22:46:18 +0200

    profiles is ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { ProfileRoutingModule } from "./profile-routing.module";
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { ProfileCardComponent } from "./profile/profile-card/profile-card.component";
+import { ProfileRoutingModule } from './profile-routing.module';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProfileCardComponent } from './profile/profile-card/profile-card.component';
 
 const routes: Routes = [
   {
-    path: "profile",
+    path: '',
     component: ProfileCardComponent
   }
 ];
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes), ProfileRoutingModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
